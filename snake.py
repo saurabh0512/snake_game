5c04914dddd4a5426d25e8794b85499bdbc8038a
saurabh0512 <43652299+saurabh0512@users.noreply.github.com>
2020-06-29 14:55:36 +0530

    background changed

--- snake.py
+++ snake.py
@@ -8,24 +8,25 @@ from tkinter import *
 import random
 
 size=300
-black_box="000000"
+snake_width=30
+colour='sienna4'
 
 class mov_tiles():
     def __init__(self):
         self.window = Tk()
         self.window.title("Snake")
-        self.canvas = Canvas(self.window , width = size , height= size)
-        self.snake = self.canvas.create_rectangle(0, 0 ,15, 15, fill='Black')
+        self.canvas = Canvas(self.window , bg='alice blue', width = size , height= size)
+        self.snake = self.canvas.create_rectangle(120, 120 ,150, 150, fill='Black')
         self.coordinates= []
         #self.coord = [i*15 for i in range(0,len(20))]
-        self.apple =self.canvas.create_rectangle(self.apple_coordinates(0, 285, 2) , fill ='Red')
+        self.my_variables=[self.canvas.coords(self.snake)]
+        self.mouse =self.canvas.create_rectangle(self.mouse_coordinates() , fill =colour)
         self.canvas.pack()
         #self.window.resizable(0,0)
         self.x = 0
         self.y = 0
         self.count = 0
         self.my_rectangles=[]
-        self.my_variables=[self.canvas.coords(self.snake)]
         self.flag = 10
         
         self.window.bind('<KeyPress-Left>' ,  self.left)
@@ -46,17 +47,21 @@ class mov_tiles():
     # ---------------------------------------
         
     def initialize_grid(self):
-        for i in range(0, 19):
-            self.canvas.create_line((i+1)*15, 0, (i+1)*15, 300)
+        for i in range(0, 9):
+            self.canvas.create_line((i+1)*(snake_width), 0, (i+1)*(snake_width), size)
         
-        for i in range(0, 19):
-            self.canvas.create_line(0, (i+1)*15, 300, (i+1)*15)
+        for i in range(0, 9):
+            self.canvas.create_line(0, (i+1)*(snake_width), size, (i+1)*(snake_width))
         
-    def apple_coordinates(self, start, end, step):
-        for i in range(step):
-            self.coordinates.append((random.randint(start,end)//15)*15)
+    def mouse_coordinates(self):
+        list1 = [(i*(snake_width), j*(snake_width)) for i in range(0, int(size/snake_width)) for j in range(0,int(size/snake_width))]
+        for i in self.my_variables:
+            if (i[0],i[1]) in list1:
+                list1.remove((i[0],i[1]))
+    
+        x,y = random.choice(list1)
+        self.coordinates.extend((x, y, x+30, y+30))
         
-        self.coordinates.extend((self.coordinates[0] + 15, self.coordinates[1] + 15))
         return self.coordinates
         
     
@@ -67,10 +72,11 @@ class mov_tiles():
 
     def initialize_game(self):
         a, b, c, d = self.canvas.coords(self.snake)
-        self.update_snake()
-        if ((a < 300 and c > 0) and (b < 300 and d > 0)):
-            #print("in this")
+        
+        if ((a < size and c > 0) and (b < size and d > 0)):
             self.canvas.move(self.snake, self.x, self.y)
+            self.update_snake()
+            
             if self.count > 0:
                 if self.canvas.coords(self.snake) in self.my_variables:
                     self.end_game()
@@ -79,50 +85,52 @@ class mov_tiles():
                     self.my_variables.append([a, b, c ,d])
                     for i in range(self.count):
                         self.move_rect(i)
-            self.canvas.after(50, self.initialize_game)
+            self.canvas.after(200, self.initialize_game)
         else :
             self.end_game()
-        #self.check_intersection()
         
     def update_snake(self):
         if  ( self.canvas.coords(self.snake) == self.coordinates):
             self.coordinates.clear()
             self.count += 1
             self.my_rectangles.append(self.canvas.create_rectangle(0, 0, 0, 0,fill='Black'))
-            #for i in range(len(self.snake_body)):
-            #    self.canvas.coords(self.temp_rect)
-            self.canvas.coords(self.apple, self.apple_coordinates(0, 285, 2))
+            self.canvas.coords(self.mouse, self.mouse_coordinates())
             
     def move_rect(self, i):
         self.my_variables.append(self.canvas.coords(self.my_rectangles[i]))
         self.canvas.coords(self.my_rectangles[i], self.my_variables[i])
         
     def end_game(self):
-        label = Label(self.canvas, text="GAME OVER")
-        label.pack()
-    
+        self.canvas.delete('all')
+        self.canvas.create_text(size/2, size/2, font="cmr 30 bold", text="GAME OVER")
+
+        self.canvas.create_text(size/2, 3*size/4, font="cmr 30 bold", text='SCORE :' + str(self.count)+'\n')
+        
+        #self.canvas.after(1000, self.window.destroy())
+        #mov_tiles()
+        
     def left(self,event):
         if not self.flag == 0 :
-            self.x=-15
+            self.x=-(snake_width)
             self.y= 0
             self.flag = 0
 
     def right(self,event):
         if not self.flag == 0:
-            self.x = 15
+            self.x = (snake_width)
             self.y = 0
             self.flag = 0
         
     def up(self,event):
         if not self.flag == 1:
             self.x= 0
-            self.y=-15
+            self.y=-(snake_width)
             self.flag = 1
         
     def down(self,event):
         if not self.flag == 1:
             self.x=0
-            self.y=15
+            self.y=(snake_width)
             self.flag = 1
             
 game=mov_tiles()
